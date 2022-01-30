60b467702df4aae3a418e39ce7ae7a54d5fa4a90
Flintin VF <flinvf@gmail.com>
2022-01-30 18:26:48 +0000

    Block Chain Secont Commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from block import Block
-block = Block("Flintin Vellara")
-block.mine(23)
+block = Block("Random String")
+# difficulty level 19!
+block.mine(19)
 print(block.hash.hexdigest())
 print(block.nonce)
 print(block.data)
